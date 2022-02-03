b9c44434104529cc51c70f8855d639b135789c9c
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-03 18:10:21 -0400

    example added

--- Practice-exercises.Test/UnitTest1.cs
+++ Practice-exercises.Test/UnitTest1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using PEMethods = Practice_exercices.Program;
+using ExercisesLib = Practice_exercices.Exercises;
 namespace Practice_exercises.Test
 {
     [TestClass]
@@ -10,17 +10,17 @@ namespace Practice_exercises.Test
         [TestMethod]
         public void AccumTest()
         {
-            Assert.AreEqual("A-Bb-Ccc-Dddd", PEMethods.Accum("abcd"));
-            Assert.AreEqual("R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy", PEMethods.Accum("RqaEzty"));
-            Assert.AreEqual("C-Ww-Aaa-Tttt", PEMethods.Accum("cwAt"));
+            Assert.AreEqual("A-Bb-Ccc-Dddd", ExercisesLib.Accum("abcd"));
+            Assert.AreEqual("R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy", ExercisesLib.Accum("RqaEzty"));
+            Assert.AreEqual("C-Ww-Aaa-Tttt", ExercisesLib.Accum("cwAt"));
         }
 
         // [TestMethod]
         public void TestLinesCounter()
         {
-            CollectionAssert.AreEqual(new List<string>() { }, PEMethods.LinesCounter(new List<string>() { }));
-            CollectionAssert.AreEqual(new List<string> { "1: a", "2: b", "3: c" }, PEMethods.LinesCounter(new List<string> { "a", "b", "c" }));
-            CollectionAssert.AreEqual(new List<string> { "1: ", "2: ", "3: ", "4: ", "5: " }, PEMethods.LinesCounter(new List<string> { "", "", "", "", "" }));
+            CollectionAssert.AreEqual(new List<string>() { }, ExercisesLib.LinesCounter(new List<string>() { }));
+            CollectionAssert.AreEqual(new List<string> { "1: a", "2: b", "3: c" }, ExercisesLib.LinesCounter(new List<string> { "a", "b", "c" }));
+            CollectionAssert.AreEqual(new List<string> { "1: ", "2: ", "3: ", "4: ", "5: " }, ExercisesLib.LinesCounter(new List<string> { "", "", "", "", "" }));
         }
 
     }
